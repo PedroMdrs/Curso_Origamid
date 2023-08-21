6521ae7056a11894186c9b8e8cdf336556681e36
Pedro Medeiros <contareservadopedro@Outlook.com>
2023-08-21 16:35:12 -0300

    f

--- Typescript/Pratica Dom/src/Slide.ts
+++ Typescript/Pratica Dom/src/Slide.ts
@@ -1,3 +1,5 @@
+import Timeout from "./TimeOut.js";
+
 export default class Slide {
   container: Element;
   slides: Element[];
@@ -5,6 +7,10 @@ export default class Slide {
   time: number;
   index: number;
   slide: Element;
+  timeout: Timeout | null;
+  pausedTimeout: Timeout | null;
+  paused: boolean;
+
   constructor(
     container: Element,
     slides: Element[],
@@ -16,8 +22,11 @@ export default class Slide {
     this.controls = controls;
     this.time = time;
 
+    this.timeout = null;
+    this.pausedTimeout = null;
     this.index = 0;
     this.slide = this.slides[this.index];
+    this.paused = false;
 
     this.init();
   }
@@ -29,15 +38,36 @@ export default class Slide {
     this.slide = this.slides[this.index];
     this.slides.forEach((el) => this.hide(el));
     this.slide.classList.add("active");
+    this.auto(this.time);
+  }
+  auto(time: number) {
+    this.timeout?.clear();
+    this.timeout = new Timeout(() => this.next(), time);
   }
   prev() {
+    if (this.paused) return;
     const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
     this.show(prev);
   }
   next() {
+    if (this.paused) return;
     const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
     this.show(next);
   }
+  pause() {
+    this.pausedTimeout = new Timeout(() => {
+      this.timeout?.pause();
+      this.paused = true;
+    }, 300);
+  }
+  continue() {
+    this.pausedTimeout?.clear();
+    if (this.paused) {
+      this.paused = false;
+      this.timeout?.continue();
+    }
+  }
+
   private addControls() {
     const prevButton = document.createElement("button");
     const nextButton = document.createElement("button");
@@ -45,6 +75,10 @@ export default class Slide {
     nextButton.innerText = "Proximo Slide";
     this.controls.appendChild(prevButton);
     this.controls.appendChild(nextButton);
+
+    this.controls.addEventListener("pointerdown", () => this.pause());
+    this.controls.addEventListener("pointerup", () => this.continue());
+
     nextButton.addEventListener("pointerup", () => this.next());
     prevButton.addEventListener("pointerup", () => this.prev());
   }

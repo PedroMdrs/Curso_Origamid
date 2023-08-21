0e6a48543953be6d6d2c0ab0410c37e0321423bf
Pedro Medeiros <contareservadopedro@Outlook.com>
2023-08-21 15:35:44 -0300

    f

--- Typescript/Pratica Dom/src/Slide.ts
+++ Typescript/Pratica Dom/src/Slide.ts
@@ -15,9 +15,11 @@ export default class Slide {
     this.slides = slides;
     this.controls = controls;
     this.time = time;
+
     this.index = 0;
-    this.show(this.index);
     this.slide = this.slides[this.index];
+
+    this.init();
   }
   hide(el: Element) {
     el.classList.remove("active");
@@ -27,5 +29,28 @@ export default class Slide {
     this.slide = this.slides[this.index];
     this.slides.forEach((el) => this.hide(el));
     this.slide.classList.add("active");
+  }
+  prev() {
+    const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+    this.show(prev);
+  }
+  next() {
+    const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
+    this.show(next);
+  }
+  private addControls() {
+    const prevButton = document.createElement("button");
+    const nextButton = document.createElement("button");
+    prevButton.innerText = "Slide Anterior";
+    nextButton.innerText = "Proximo Slide";
+    this.controls.appendChild(prevButton);
+    this.controls.appendChild(nextButton);
+    nextButton.addEventListener("pointerup", () => this.next());
+    prevButton.addEventListener("pointerup", () => this.prev());
+  }
+
+  private init() {
+    this.addControls();
+    this.show(this.index);
   }
 }
--- Typescript/Pratica Dom/src/script.ts
+++ Typescript/Pratica Dom/src/script.ts
@@ -10,5 +10,4 @@ if (container && elements && controls && elements.children.length) {
     controls,
     3000
   );
-  slide.show(3);
 }

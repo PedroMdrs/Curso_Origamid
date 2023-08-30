1e94bafc6d4b9e4d658078baf956e4a4352deeab
Pedro Medeiros <contareservadopedro@Outlook.com>
2023-08-30 15:26:56 -0300

    Projeto Fintech

--- React com typescript/Projeto Fintech/src/Components/DateInput.tsx
+++ React com typescript/Projeto Fintech/src/Components/DateInput.tsx
@@ -3,11 +3,40 @@ import React from "react";
 type IDateInput = React.ComponentProps<"input"> & {
   label: string;
 };
+
+const generalStyle: React.CSSProperties = {
+  fontSize: "1rem",
+  color: "var(--color-2)",
+  padding: "var(--gap-s) .75rem",
+  backgroundColor: "var(--color-4)",
+  borderRadius: "var(--gap)",
+};
+const labelStyle: React.CSSProperties = {
+  display: "block",
+  marginBottom: "var(--gap-s)",
+  fontWeight: "600",
+  ...generalStyle,
+};
+
+const inputStyle: React.CSSProperties = {
+  border: "none",
+  fontFamily: "monospace",
+  ...generalStyle,
+};
+
 const DateInput = ({ label, ...props }: IDateInput) => {
   return (
     <div>
-      <label htmlFor={label}>{label}</label>
-      <input name={label} type="date" id="inicio" {...props} />
+      <label style={labelStyle} htmlFor={label}>
+        {label}
+      </label>
+      <input
+        style={inputStyle}
+        name={label}
+        type="date"
+        id={label}
+        {...props}
+      />
     </div>
   );
 };

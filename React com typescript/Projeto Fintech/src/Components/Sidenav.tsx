9f8cbdf46f52c9d09a16c8bddcb59a6039e63d1a
Pedro Medeiros <contareservadopedro@Outlook.com>
2023-08-31 15:32:48 -0300

    sidenav

--- React com typescript/Projeto Fintech/src/Components/Sidenav.tsx
+++ React com typescript/Projeto Fintech/src/Components/Sidenav.tsx
@@ -1,7 +1,56 @@
 import React from "react";
+import FintechSVG from "../assets/FintechSVG";
+import Resumo from "../assets/icons/resumo.svg";
+import Vendas from "../assets/icons/vendas.svg";
+import webhooks from "../assets/icons/webhooks.svg";
+import configuracoes from "../assets/icons/configuracoes.svg";
+import contato from "../assets/icons/contato.svg";
+import sair from "../assets/icons/sair.svg";
 
 const Sidenav = () => {
-  return <div>Sidenav</div>;
+  return (
+    <nav className="sidenav box bg-3">
+      <FintechSVG title="Fintech logo" />
+      <ul>
+        <li>
+          <span>
+            <img src={Resumo} alt="" />
+          </span>
+          <a href="">Resumo</a>
+        </li>
+        <li>
+          <span>
+            <img src={Vendas} alt="" />
+          </span>
+          <a href="">Vendas</a>
+        </li>
+        <li>
+          <span>
+            <img src={webhooks} alt="" />
+          </span>
+          <a>WebHooks</a>
+        </li>
+        <li>
+          <span>
+            <img src={configuracoes} alt="" />
+          </span>
+          <a>Configuracoes</a>
+        </li>
+        <li>
+          <span>
+            <img src={contato} alt="" />
+          </span>
+          <a>Contato</a>
+        </li>
+        <li>
+          <span>
+            <img src={sair} alt="" />
+          </span>
+          <a>Sair</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Sidenav;

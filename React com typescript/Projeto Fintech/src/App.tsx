358a764591342c8cf679149c05382baa87e87ff4
Pedro Medeiros <contareservadopedro@Outlook.com>
2023-08-31 15:43:04 -0300

    Venda router

--- React com typescript/Projeto Fintech/src/App.tsx
+++ React com typescript/Projeto Fintech/src/App.tsx
@@ -5,18 +5,27 @@ import Sidenav from "./Components/Sidenav";
 import Header from "./Components/Header";
 import { DataContextProvider } from "./Context/DataContext";
 import Vendas from "./Pages/Vendas";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Venda from "./Pages/Venda";
 const App = () => {
   return (
-    <DataContextProvider>
-      <div className="container">
-        <Sidenav></Sidenav>
-        <main>
-          <Header></Header>
-          <Resumo></Resumo>
-          <Vendas></Vendas>
-        </main>
-      </div>
-    </DataContextProvider>
+    <BrowserRouter>
+      <DataContextProvider>
+        <div className="container">
+          <Sidenav></Sidenav>
+          <main>
+            <Header></Header>
+            <Routes>
+              <Route path="/" element={<Resumo />} />
+              <Route path="/vendas" element={<Vendas />} />
+              <Route path="/vendas/:id" element={<Venda />} />
+            </Routes>
+            <Resumo></Resumo>
+            <Vendas></Vendas>
+          </main>
+        </div>
+      </DataContextProvider>
+    </BrowserRouter>
   );
 };
 
--- React com typescript/Projeto Fintech/src/Components/Sidenav.tsx
+++ React com typescript/Projeto Fintech/src/Components/Sidenav.tsx
@@ -6,6 +6,7 @@ import webhooks from "../assets/icons/webhooks.svg";
 import configuracoes from "../assets/icons/configuracoes.svg";
 import contato from "../assets/icons/contato.svg";
 import sair from "../assets/icons/sair.svg";
+import { NavLink } from "react-router-dom";
 
 const Sidenav = () => {
   return (
@@ -16,13 +17,13 @@ const Sidenav = () => {
           <span>
             <img src={Resumo} alt="" />
           </span>
-          <a href="">Resumo</a>
+          <NavLink to="/">Resumo</NavLink>
         </li>
         <li>
           <span>
             <img src={Vendas} alt="" />
           </span>
-          <a href="">Vendas</a>
+          <NavLink to="/vendas">Vendas</NavLink>
         </li>
         <li>
           <span>
